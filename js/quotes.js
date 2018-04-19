287e71b21ec6af44c3ba5d37a6aa73caab1e9b46
Aaron Gibson <aaronwgibson86@gmail.com>
2018-04-18 19:28:31 -0500

    Added quotes JS file/updated index.html to include extra JS file

---
+++ js/quotes.js
@@ -0,0 +1,51 @@
+//Created seperate file to store quotes to keep the main code cleaner.
+
+var quotes = [
+    {
+        quote: 'Nobody exists on purpose, nobody belongs anywhere, everybody\'s gonna die. Come watch TV.',
+        source: 'Morty',
+        citation: 'Rick and Morty'
+    },
+    {
+        quote: 'Life would be tragic if it weren\'t funny.',
+        source: 'Stephen Hawking',
+        citation: 'The New York Times',
+        year: 2004
+    },
+    {
+        quote: 'A day without sunshine is like, you know, night.',
+        source: 'Steve Martin'
+    },
+    {
+        quote: 'I\'m sorry, if you were right, I\'d agree with you.',
+        source: 'Robin Williams'
+    },
+    {
+        quote: 'All generalizations are false, including this one.',
+        source: 'Mark Twain'
+    },
+    {
+        quote: 'War is God\'s way of teaching Americans geography.',
+        source: 'Ambrose Bierce',
+        citation: 'The Violent Foam: New and Selected Poems',
+        year: 2002
+    },
+    {
+        quote: 'Impressive. Every word in that sentence was wrong.',
+        source: 'Luke Skywalker',
+        citation: 'Star Wars: Episode VIII - The Last Jedi',
+        year: 2017
+    },
+    {
+        quote: 'I am the one who knocks!',
+        source: 'Walter White',
+        citation: 'Breaking Bad',
+        year: 2011
+    },
+    {
+        quote: 'I hope I didn\'t brain my damage.',
+        source: 'Homer Simpson',
+        citation: 'The Simpsons',
+        year: 1996
+    }
+];
