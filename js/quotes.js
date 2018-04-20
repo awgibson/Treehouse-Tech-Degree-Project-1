c818c2be500648f85b1f8db1c4bfcbe39a2a13ca
Aaron Gibson <aaronwgibson86@gmail.com>
2018-04-19 19:36:07 -0500

    Changed variable names/added tags/moved duplicate quote loop to getRandomQuote function

--- js/quotes.js
+++ js/quotes.js
@@ -1,51 +1,59 @@
 //Created seperate file to store quotes to keep the main code cleaner.
-
 var quotes = [
     {
         quote: 'Nobody exists on purpose, nobody belongs anywhere, everybody\'s gonna die. Come watch TV.',
         source: 'Morty',
-        citation: 'Rick and Morty'
+        citation: 'Rick and Morty',
+        tags: ['TV', 'Humor', 'Entertainment']
     },
     {
         quote: 'Life would be tragic if it weren\'t funny.',
         source: 'Stephen Hawking',
         citation: 'The New York Times',
-        year: 2004
+        year: 2004,
+        tags: ['Scientist', 'Life']
     },
     {
         quote: 'A day without sunshine is like, you know, night.',
-        source: 'Steve Martin'
+        source: 'Steve Martin',
+        tags: ['Humor', 'Comedian']
     },
     {
         quote: 'I\'m sorry, if you were right, I\'d agree with you.',
-        source: 'Robin Williams'
+        source: 'Robin Williams',
+        tags: ['Humor', 'Comedian', 'Comeback']
     },
     {
         quote: 'All generalizations are false, including this one.',
-        source: 'Mark Twain'
+        source: 'Mark Twain',
+        tags: ['Literature', 'Humor', 'Witty']
     },
     {
         quote: 'War is God\'s way of teaching Americans geography.',
         source: 'Ambrose Bierce',
         citation: 'The Violent Foam: New and Selected Poems',
-        year: 2002
+        year: 2002,
+        tags: ['Poetry', 'War', 'Politcal']
     },
     {
         quote: 'Impressive. Every word in that sentence was wrong.',
         source: 'Luke Skywalker',
         citation: 'Star Wars: Episode VIII - The Last Jedi',
-        year: 2017
+        year: 2017,
+        tags: ['Film', 'Entertainment', 'Comeback']
     },
     {
         quote: 'I am the one who knocks!',
         source: 'Walter White',
         citation: 'Breaking Bad',
-        year: 2011
+        year: 2011,
+        tags: ['TV', 'Drama']
     },
     {
         quote: 'I hope I didn\'t brain my damage.',
         source: 'Homer Simpson',
         citation: 'The Simpsons',
-        year: 1996
+        year: 1996,
+        tags: ['Silly', 'TV', 'Humor']
     }
 ];
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-var randomQuote;
-var lastQuote = '';
+//Define variables
+var currentQuote;
+var previousQuote = '';
 var buildQuote;
 var quoteBox = document.getElementById('quote-box');
 var rgb;
@@ -10,51 +11,59 @@ var autoQuote;
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-//Random number function I can call in other functions to make the code a little neater
+//Random number function to help keep code cleaner
 function randNum(low, high) {
     return Math.floor(Math.random() * high) + low;
 }
 
 //Generates a random numbers from 0 to 255 using the randNum function and changes the body background to the generated string.
-function randRGB() {
+function randomBackground() {
     rgb = 'rgb(' + randNum(0, 255) + ',' + randNum(0, 255) + ',' + randNum(0, 255) + ')';
     document.body.style.backgroundColor = rgb;
 }
 
 function getRandomQuote() {
-    return quotes[randNum(0, quotes.length)];
+    // Picks a random quote
+    currentQuote = quotes[randNum(0, quotes.length)];
+
+    //Prevents a quote from being displayed twice in a row
+    while (currentQuote === previousQuote) {
+        currentQuote = getRandomQuote();
+        console.log('Duplicate quote! Generating a new quote!');
+    }
+    return currentQuote;
 }
 
 function printQuote() {
-
-    //When the function runs, the interval is cleared for the autoQuote and then reset for 30 seconds.
+    //When the function runs, the interval is cleared for the autoQuote and then set for 30 seconds.
     clearInterval(autoQuote);
     autoQuote = setInterval(function () { printQuote(); }, 30000);
 
-    randomQuote = getRandomQuote();
+    //Calls function and returns the currentQuote variable.
+    getRandomQuote();
 
-    //If the same quote is used twice in a row, this loop will run until it finds different quote.
-    while (randomQuote === lastQuote) {
-        randomQuote = getRandomQuote();
-        console.log('Duplicate quote! Generating a new quote!');
-    }
-
-    //Build the displayed text for the quote
+    //Build the displayed text for the quote with conditionals to add the citation, year, and tags (hidden) if present
     buildQuote = '';
-    buildQuote += '<p class="quote">' + randomQuote.quote + '</p>';
-    buildQuote += '<p class="source">' + randomQuote.source;
-    if (randomQuote.citation) {
-        buildQuote += '<span class="citation">' + randomQuote.citation + '</span>';
+    buildQuote += '<p class="quote">' + currentQuote.quote + '</p>';
+    buildQuote += '<p class="source">' + currentQuote.source;
+    if (currentQuote.citation) {
+        buildQuote += '<span class="citation">' + currentQuote.citation + '</span>';
     }
-    if (randomQuote.year) {
-        buildQuote += '<span class="year">' + randomQuote.year + '</span>';
+    if (currentQuote.year) {
+        buildQuote += '<span class="year">' + currentQuote.year + '</span>';
     }
 
-    // Display quote, call the random background color function, set the lastQuote variable 
-    //equal to the current randomQuote to compare the next time the function is called
+    //Added a hidden span for the tags and a class attribute of tags that is currently not styled so the
+    //tags may be added in and styled at a later point if desired
+    if (currentQuote.tags) {
+        buildQuote += '<span class="tags" style="visibility: hidden">' + currentQuote.tags.join(', ') + '</span>';
+    }
+
+    // Display quote, call the random background color function, set the previousQuote variable 
+    //equal to the currentQuote to compare the next time the function is called
     quoteBox.innerHTML = buildQuote;
-    randRGB();
-    lastQuote = randomQuote;
+    randomBackground();
+    previousQuote = currentQuote;
 }
 
 //call printQuote as soon as page loads
